5e0374c77126b38f0abbf13e1d5823381b1686f8
Jessica Brown <jessicakbrown1120@gmail.com>
2017-09-27 18:19:51 -0500

    initial bugs fixed, created more on my own

--- public/scripts/controllers/peeps.controller.js
+++ public/scripts/controllers/peeps.controller.js
@@ -1,15 +1,23 @@
-myApp.controller( 'WhereMyPeeps', function( $http, PeepsService ){
-var vm = this;
-vm.newPeep = function(){
-console.log( 'in newPeep' );
-var peepToAdd = {
-name= vm.nameIn,
-location= vm.locationIn
-}; //end peepToAdd
-PeepsService.peeps.add( peepToAdd );
-}; //end whereMyPeepsAt
-vm.whereMyPeepsAt = function(){
-console.log( 'in whereMyPeepsAt' );
-PeepsService.peeps.get();
-}; //end whereMyPeepsAt
+myApp.controller('WhereMyPeeps', function (PeepsService) {
+    var vm = this;
+
+    //array of results from server
+    vm.allPeeps = [];
+    
+    //populates the DOM on load w/ peeps in Server
+    PeepsService.get();
+    // tells wmp controller to get the peeps from Server from the Peeps Service
+    vm.peepsFromServer = PeepsService.peepsFromServer 
+    console.log('peeps', vm.peepsFromServer);
+    
+    vm.newPeep = function () {
+        console.log('in newPeep');
+        var peepToAdd = {
+            name: vm.nameIn,
+            location: vm.locationIn
+        }; //end peepToAdd
+        PeepsService.add(peepToAdd);
+        PeepsService.get();
+    }; 
+    
 }); // end controller
